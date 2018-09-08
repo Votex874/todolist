c4987e554f93c7e2c7080e3d4eb7d51543050818
Sebastian <votex999@gmail.com>
2018-09-08 16:31:21 +0200

    .

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -8,9 +8,10 @@ class ToDoList extends Component {
         super(props);
 
         let localTasks = [];
-        // let tasks = JSON.parse(localStorage.tasks);
-        // let falseArray = this.createFalseArray(tasks);
-        // let importantArray = this.createFalseArray(tasks);
+        localStorage.setItem('tasks', JSON.stringify(localTasks));
+        let tasks = JSON.parse(localStorage.tasks);
+        let falseArray = this.createFalseArray(tasks);
+        let importantArray = this.createFalseArray(tasks);
         let displayTooltip = {
             visibility: 'hidden',
         };
